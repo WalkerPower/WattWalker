3edb1f8b6d0135bf6dfa3300fffed3afbf5b6d7d
Jasmine Walker <jasminewalker@Jasmines-MacBook-Air-2.local>
2026-03-03 02:54:31 -0500

    iOS setup and configuration fixes from MacBook

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.walkerpower.wattwalker',
+  appName: 'WattWalker',
+  webDir: 'dist'
+};
+
+export default config;
